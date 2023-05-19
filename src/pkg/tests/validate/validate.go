9b9721601c0ce230e122434c64d579a301a8783e
Ben Wright <benjamin.p.wright@hotmail.com>
2023-05-19 22:36:04 +0100

    WIP: support nested maps / arrays in validation

--- src/pkg/tests/validate/validate.go
+++ src/pkg/tests/validate/validate.go
@@ -11,6 +11,38 @@ import (
 
 var UUID_REGEX = regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`)
 
+func setIgnoredFields(t *testing.T, typ reflect.Type, expected reflect.Value, actual reflect.Value) {
+	numFields := typ.NumField()
+	for i := 0; i < numFields; i++ {
+		field := typ.Field(i)
+		tag := field.Tag.Get("validate")
+
+		if tag == "" {
+			assert.Equal(t, expected.FieldByName(field.Name).Interface(), actual.FieldByName(field.Name).Interface(), "fields should match: %s", field.Name)
+			continue
+		}
+
+		keys := strings.Split(tag, ",")
+		for _, key := range keys {
+			switch key {
+			case "uuid":
+				value := actual.FieldByName(field.Name)
+				if value.Kind() != reflect.String {
+					assert.Fail(t, "non-string field %s (%s) marked as a UUID for validation", field.Name, value.Kind())
+					continue
+				}
+
+				expected.FieldByName(field.Name).SetString("a valid UUID")
+
+				valueStr := value.String()
+				if UUID_REGEX.Match([]byte(valueStr)) {
+					value.SetString("a valid UUID")
+				}
+			}
+		}
+	}
+}
+
 func Equal[T any](t *testing.T, expected T, actual T) bool {
 	ptrTyp := reflect.TypeOf(expected)
 	if !assert.Equal(t, ptrTyp.Kind(), reflect.Pointer, "expecting a pointer to a struct") {
@@ -25,35 +57,7 @@ func Equal[T any](t *testing.T, expected T, actual T) bool {
 	expectedVal := reflect.ValueOf(expected).Elem()
 	actualVal := reflect.ValueOf(actual).Elem()
 
-	numFields := typ.NumField()
-	for i := 0; i < numFields; i++ {
-		field := typ.Field(i)
-		tag := field.Tag.Get("validate")
-
-		if tag == "" {
-			assert.Equal(t, expectedVal.FieldByName(field.Name).Interface(), actualVal.FieldByName(field.Name).Interface(), "fields should match: %s", field.Name)
-			continue
-		}
-
-		keys := strings.Split(tag, ",")
-		for _, key := range keys {
-			switch key {
-			case "uuid":
-				value := actualVal.FieldByName(field.Name)
-				if value.Kind() != reflect.String {
-					assert.Fail(t, "non-string field %s (%s) marked as a UUID for validation", field.Name, value.Kind())
-					continue
-				}
-
-				expectedVal.FieldByName(field.Name).SetString("a valid UUID")
-
-				valueStr := value.String()
-				if UUID_REGEX.Match([]byte(valueStr)) {
-					value.SetString("a valid UUID")
-				}
-			}
-		}
-	}
+	setIgnoredFields(t, typ, expectedVal, actualVal)
 
 	return assert.Equal(t, expected, actual)
 }
